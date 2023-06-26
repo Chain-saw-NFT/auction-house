a0b6e60e4eb3484c1a323e25d53a0ae6906e6ecf
0xStarVoyager <0xStarVoyager@gmail.com>
2023-06-26 14:00:15 -0500

    updates hardhat config for verification

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,14 +1,13 @@
-import { task } from "hardhat/config";
+import { config } from "dotenv";
 import "@nomiclabs/hardhat-waffle";
 import "@nomiclabs/hardhat-ethers";
 import "hardhat-typechain";
 import "solidity-coverage";
 import "@nomiclabs/hardhat-etherscan";
-import "hardhat-gas-reporter"
-import 'hardhat-abi-exporter';
+import "hardhat-gas-reporter";
+import "hardhat-abi-exporter";
 
-
-  
+config({ path: `.env.${process.env.NODE_ENV || "local"}` });
 
 /**
  * @type import('hardhat/config').HardhatUserConfig
@@ -21,20 +20,27 @@ export default {
       },
       {
         version: "0.8.0",
-      }
+      },
     ],
   },
+  networks: {
+    mainnet: {
+      url: `https://mainnet.infura.io/v3/${process.env.MAINNET_API_KEY}`,
+    },
+  },
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API_KEY,
+  },
   abiExporter: {
-    path: './data/abi',
+    path: "./data/abi",
     clear: true,
     flat: true,
     spacing: 2,
     pretty: true,
   },
   gasReporter: {
-    currency: 'USD',
+    currency: "USD",
     gasPrice: 100,
     coinmarketcap: process.env.COINMARKETCAP_API_KEY,
-  }
-  
+  },
 };
